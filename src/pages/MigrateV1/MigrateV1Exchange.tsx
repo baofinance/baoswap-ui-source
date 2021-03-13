bfc03d34422df4ad9cb176637c6a8ca7e64751ac
Vitalsine85 <34991917+Vitalsine85@users.noreply.github.com>
2021-03-13 11:33:42 -0500

    Update MigrateV1Exchange.tsx

--- src/pages/MigrateV1/MigrateV1Exchange.tsx
+++ src/pages/MigrateV1/MigrateV1Exchange.tsx
@@ -205,8 +205,7 @@ function V1PairMigration({ liquidityTokenAmount, token }: { liquidityTokenAmount
       {!isFirstLiquidityProvider && largePriceDifference ? (
         <YellowCard>
           <TYPE.body style={{ marginBottom: 8, fontWeight: 400 }}>
-            It{"'"}s best to deposit liquidity into BaoSwap V2 at a price you believe is correct. If the V2 price
-            seems incorrect, you can either make a swap to move the price or wait for someone else to do so.
+            It{"'"}s best to deposit liquidity into BaoSwap V2 at a price you believe is correct. If the V2 price seems incorrect, you can either make a swap to move the price or wait for someone else to do so.
           </TYPE.body>
           <AutoColumn gap="8px">
             <RowBetween>
@@ -246,8 +245,7 @@ function V1PairMigration({ liquidityTokenAmount, token }: { liquidityTokenAmount
       {isFirstLiquidityProvider && (
         <PinkCard>
           <TYPE.body style={{ marginBottom: 8, fontWeight: 400 }}>
-            You are the first liquidity provider for this pair on BaoSwap V2. Your liquidity will be migrated at the
-            current V1 price. Your transaction cost also includes the gas to create the pool.
+            You are the first liquidity provider for this pair on BaoSwap V2. Your liquidity will be migrated at the current V1 price. Your transaction cost also includes the gas to create the pool.
           </TYPE.body>
 
           <AutoColumn gap="8px">
@@ -360,8 +358,7 @@ export default function MigrateV1Exchange({
         ) : validatedAddress && chainId && token?.equals(WETH[chainId]) ? (
           <>
             <TYPE.body my={9} style={{ fontWeight: 400 }}>
-              Because BaoSwap V2 uses WETH under the hood, your BaoSwap V1 WETH/ETH liquidity cannot be migrated.
-              You may want to remove your liquidity instead.
+              Because BaoSwap V2 uses WETH under the hood, your BaoSwap V1 WETH/ETH liquidity cannot be migrated. You may want to remove your liquidity instead.
             </TYPE.body>
 
             <ButtonConfirmed
